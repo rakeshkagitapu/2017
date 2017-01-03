7b5ce5f37b5fc19c9641212683b8f386e058c156
rakeshkagitapu <rakeshkagitapu@gmail.com>
2017-01-03 10:17:55 -0500

    commit

---
+++ SPFS/Scripts/Validation.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    
+    $('.decimalNumeric').keyup(function () {
+
+        if (this.value.match(/[^(10|\d)(\.\d{1,2})?]/g)) {
+            this.value = this.value.replace(/[^(10|\d)(\.\d{1,2})?]/g, '');
+        }
+    });
+
+    $('.onlyNumeric').keyup(function () {
+
+        if (this.value.match(/[^0-9]/g)) {
+            this.value = this.value.replace(/[^0-9]/g, '');
+        }
+    });
+    //validation
+    jQuery.validator.addMethod(
+         "NumberGreaterZero",
+         function (value, element) {
+             var check = false;
+             if (value > 0) {
+                 check = true;
+             } else
+                 check = false;
+             return this.optional(element) || check;
+         },
+         "Value should be greater than 0"
+     );
+});
